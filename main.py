f07833427a52cf51c750989127d62994e73bfdf6
José Henrique <jose.l.pellet@gmail.com>
2022-03-20 15:01:15 -0300

    Função Extrair Audio adicionada
    Bugs ao cortar video e função de extrair audio adicionada

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 from editor import Editor
 from unidecode import unidecode
+import os
 
 editor = Editor()
 
@@ -12,6 +13,10 @@ def open_file():
         (("MP4 files","*.mp4"),("all files","*.*")) )
 
     if file_path != '':
+        file_size = os.path.getsize(file_path)
+        file_size = file_size *1000
+        root.MsgSizeFile["text"] = "Tamanho do arquivo: "+str(round(file_size/(1000000000)))+"MB"
+
         editor.setVideoPath(file_path)
         file_path_arr = file_path.split('/')
         indice = len(file_path_arr) - 1
@@ -19,12 +24,20 @@ def open_file():
         editor.setVideoName(unidecode(file_path_arr[indice][:-4]))
         root.BtnSelectFile["text"] = file_path_arr[indice]
 
+def audioEstractStatus():
+    if root.extractAudioBtn["text"]  == "NÃO":
+        root.extractAudioBtn["text"] = "SIM"
+        editor.setExtractAudio(True)
+    else:
+        root.extractAudioBtn["text"] = "NÃO"
+        editor.setExtractAudio(False)
+
 def startEdition():
     editor.setVideoCuts(root.CutsInput.get())
     if editor.startEdition() == True:
         print('Sucesso')
         root.LabelError["fg"] = "#0eab20"
-        root.LabelError["text"] = "Sucesso: Video cortado com sucesso!"
+        root.LabelError["text"] = "Sucesso: Video editado com sucesso!"
     else:
         print('Erro')
         root.LabelError["fg"] = "#c40424"
@@ -53,6 +66,11 @@ root.SelectFile = Frame(root)
 root.SelectFile["pady"] = 10
 root.SelectFile["width"] = 800
 root.SelectFile.pack()
+
+root.MsgSize = Frame(root)
+root.MsgSize["pady"] = 10
+root.MsgSize["width"] = 800
+root.MsgSize.pack()
 
 root.SelectOptions = Frame(root)
 root.SelectOptions["pady"] = 10
@@ -84,6 +102,11 @@ root.BtnSelectFile["fg"] = ("#30138f")
 root.BtnSelectFile["command"] = lambda:open_file()
 root.BtnSelectFile.pack(side="left")
 
+root.MsgSizeFile = Label(root.MsgSize, text="", font=root.fontePadrao)
+root.MsgSizeFile["font"] = root.fontePadrao
+root.MsgSizeFile["width"] = 90
+root.MsgSizeFile.grid(row=0, column=3, padx=10, pady=10)
+
 video_label = Label(root.BtnSelectFile)
 
 root.CutsLabel = Label(root.SelectOptions, text="Número de Cortes: ", font=root.fontePadrao)
@@ -96,6 +119,20 @@ root.CutsInput = Entry(root.SelectOptions)
 root.CutsInput["font"] = root.fontePadrao
 root.CutsInput["width"] = 20
 root.CutsInput.grid(row=0, column=2, padx=10, pady=10)
+
+root.extractAudioLabel = Label(root.SelectOptions, text="Extrair Áudio: ", font=root.fontePadrao)
+root.extractAudioLabel["font"] = root.fontePadrao
+root.extractAudioLabel["width"] = 15
+root.extractAudioLabel.grid(row=0, column=3, padx=10, pady=10)
+
+root.extractAudioBtn = Button(root.SelectOptions)
+root.extractAudioBtn["text"] = "NÃO"
+root.extractAudioBtn["font"] = root.fontePadrao
+root.extractAudioBtn["width"] = 10
+root.extractAudioBtn.grid(row=0, column=4, padx=10, pady=10)
+root.extractAudioBtn["font"] = ("bold")
+root.extractAudioBtn["fg"] = ("#30138f")
+root.extractAudioBtn["command"] = lambda:audioEstractStatus()
 
 root.BtnStart = Button(root.FinalOptions)
 root.BtnStart["text"] = "Iniciar"
@@ -121,8 +158,6 @@ root.LabelError["font"] = root.fontePadrao
 root.LabelError["font"] = ("bold")
 root.LabelError["width"] = 50
 root.LabelError.grid(row=1, column=3, padx=20, pady=20)
-
-
 
 root.titulo.pack()
 root.mainloop()
